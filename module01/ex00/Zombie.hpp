e8bedfa846521abd14aa25ed5d73ed2cdd153cec
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-10-13 19:32:42 +0100

    moved file to this directory

---
+++ module01/ex00/Zombie.hpp
@@ -0,0 +1,15 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+#include <iostream>
+#define _string std::string
+class Zombie{
+        _string name;
+    public:
+        Zombie(_string _name);
+        ~Zombie();
+        void announce(void);
+};
+
+Zombie *newZombie(_string name);
+void randomChump(_string name);
+#endif
