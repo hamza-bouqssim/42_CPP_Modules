311b0c3266e3e30f80a24cbfcac8a3213c2cc305
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-10-16 20:20:17 +0100

    setting the ground

---
+++ module01/ex03/Weapon.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 19:31:21 by hbouqssi          #+#    #+#             */
+/*   Updated: 2022/10/16 20:19:14 by hbouqssi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+#include <iostream>
+#define _string std::string
+
+class Weapon{
+        _string type;
+    public:
+        
+        
+};
+
+#endif
