5f164156c56f211d65ee315c77f6d33ad1a0766c
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-10-30 23:27:39 +0100

    not done yet & adding headers

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 22:15:20 by hbouqssi          #+#    #+#             */
+/*   Updated: 2022/10/30 23:22:35 by hbouqssi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <fstream>
 
@@ -8,10 +20,12 @@ int main(int argc, char **argv)
         std::cout << "Verify Your arguments !!!" << std::endl;
         return 0;
     }
+
     std::string filename = argv[1];
     std::string s1 = argv[2];
     std::string s2 = argv[3];
     std::ifstream inFile(filename);
+    
     if (!inFile)
     {
         std::cout << "a file with this name - " << argv[1] << " - is not exist !" << std::endl;
@@ -23,12 +37,18 @@ int main(int argc, char **argv)
         std::cout << "Failed !" << std::endl;
         return 0;
     }
-
     std::string lines, jLines = "", str;
     while(getline(inFile, lines))
         jLines += lines + '\n';
-    int i = jLines.find(s1);
-    str = jLines.substr(0, i) + s2 + jLines.substr(i + s1.length(), -1);
-    OutFile << str; 
-    
+    while (true)
+    {
+        int i = jLines.find(s1, s1.length());
+        if (i == -1)
+            break ;
+        str = jLines.substr(0, i) + s2 + jLines.substr(i + s1.length(), -1);
+        jLines = str;
+    }
+    OutFile << str;
+    inFile.close();
+    OutFile.close();
 }
