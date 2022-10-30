23242147e50dac5aa8a552fcb129805bab737e24
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-10-31 00:17:48 +0100

    handled a case, there still some issues

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 22:15:20 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/10/30 23:22:35 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/10/31 00:11:13 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,19 @@ int main(int argc, char **argv)
     std::string s1 = argv[2];
     std::string s2 = argv[3];
     std::ifstream inFile(filename);
-    
+    std::string lines, jLines = "", str;
+    if(s1 == s2)
+    {
+       std::ofstream OutpFile(filename + ".replace");
+       if(!OutpFile)
+       {
+        std::cout << "Failed" << std::endl;
+        return 0;
+       }
+       while(getline(inFile, lines))
+            OutpFile << lines + '\n';
+        return 0;
+    }
     if (!inFile)
     {
         std::cout << "a file with this name - " << argv[1] << " - is not exist !" << std::endl;
@@ -37,18 +49,15 @@ int main(int argc, char **argv)
         std::cout << "Failed !" << std::endl;
         return 0;
     }
-    std::string lines, jLines = "", str;
     while(getline(inFile, lines))
         jLines += lines + '\n';
     while (true)
     {
-        int i = jLines.find(s1, s1.length());
+        int i = jLines.find(s1);
         if (i == -1)
             break ;
         str = jLines.substr(0, i) + s2 + jLines.substr(i + s1.length(), -1);
         jLines = str;
     }
     OutFile << str;
-    inFile.close();
-    OutFile.close();
 }
