a58e037f4d3bbcb7ca99c6d659fcb630650ad9d4
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-11-01 02:21:22 +0100

    Problem Solved! && ex04:finished

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,15 +6,17 @@
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 22:15:20 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/10/31 00:11:13 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/11/01 02:19:34 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 
 int main(int argc, char **argv)
 {
+	zac *l;
     if (argc != 4)
     {
         std::cout << "Verify Your arguments !!!" << std::endl;
@@ -22,25 +24,13 @@ int main(int argc, char **argv)
     }
 
     std::string filename = argv[1];
-    std::string s1 = argv[2];
+    std::string s1(argv[2]);
     std::string s2 = argv[3];
     std::ifstream inFile(filename);
     std::string lines, jLines = "", str;
     if(s1.empty() || s2.empty())
     {
         std::cout << "Empty String !" << std::endl;
-        return 0;
-    }
-    if(s1 == s2)
-    {
-       std::ofstream OutpFile(filename + ".replace");
-       if(!OutpFile)
-       {
-        std::cout << "Failed" << std::endl;
-        return 0;
-       }
-       while(getline(inFile, lines))
-            OutpFile << lines + '\n';
         return 0;
     }
     if (!inFile)
@@ -54,17 +44,16 @@ int main(int argc, char **argv)
         std::cout << "Failed !" << std::endl;
         return 0;
     }
-    while (getline(inFile, lines))
-        jLines += lines + '\n';
-    int i = 0;
-    while (i < (int)jLines.length())
-    {
-
-        i = jLines.find(s1);
-        std::string erased = jLines.erase(i, s1.end());
-        str = erased.substr(0, i) + s2 + jLines.substr(i + s1.length(), -1);
-        erased = str;
-        i++;
-    }
-    OutFile << str;
+	std::string erased;
+	size_t i = 0;
+    while (getline(inFile, jLines, '\0'))
+	{
+		while ((i = jLines.find(s1 ,i)) && i != (size_t)-1)
+		{
+			jLines.erase(i, s1.length());
+			jLines.insert(i, s2);
+			i += s2.length();
+		}
+		OutFile << jLines;
+	}
 }
