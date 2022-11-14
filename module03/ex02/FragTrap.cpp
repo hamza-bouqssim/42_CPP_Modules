b36ca65f66241c2fced53da09290619aa03889c2
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-11-14 13:15:47 +0100

    main added

--- module03/ex02/FragTrap.cpp
+++ module03/ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 12:12:50 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/11/14 12:51:29 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/11/14 13:06:06 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ FragTrap::FragTrap(): ClapTrap(){
 };
 
 FragTrap::FragTrap(std::string _name): ClapTrap(_name){
-    std::cout << "FragTrap Constructor Called" << std::endl;
+    std::cout << "FragTrap Parametrized Constructor Called" << std::endl;
     this->name = _name;
     this->Hit_Points = 100;
     this->Energy_point = 100;
@@ -34,7 +34,10 @@ FragTrap::FragTrap(std::string _name): ClapTrap(_name){
 FragTrap::FragTrap(const FragTrap &_FragTrap)
 {
     std::cout << "FragTrap Copy Constructor Called" << std::endl;
-    *this = _FragTrap;
+    this->name = _FragTrap.name;
+    this->Hit_Points = _FragTrap.Hit_Points;
+    this->Energy_point = _FragTrap.Energy_point;
+    this->Attack_damage = _FragTrap.Attack_damage;
 };
 
 FragTrap& FragTrap::operator=(const FragTrap &_FragTrap)
--- module03/ex02/main.cpp
+++ module03/ex02/main.cpp
@@ -6,12 +6,26 @@
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 12:02:32 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/11/14 12:50:06 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/11/14 13:15:14 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
+#define N 97
+#define NAME "UNKNOWN"
 int main()
 {
+    FragTrap _FragTrap = FragTrap("Hamza");
+    _FragTrap.attack("Unknown");
+    _FragTrap.attack("Unknown");
+    _FragTrap.attack("Unknown");
+    std::cout << "should attack " << NAME  << " " << N << " time" << std::endl;
+    for(int i = 0; i < N; i++)
+    {
+        _FragTrap.attack(NAME);
+    }
+    std::cout << "Should not diplay the hive five Message" << std::endl;
+    _FragTrap.highFivesGuys();
+    
     return 0;
 }
