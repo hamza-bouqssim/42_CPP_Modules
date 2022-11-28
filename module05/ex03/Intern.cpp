b37e554063d6f494dac537f9ee5c33902e99d0e9
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-11-28 19:52:49 +0100

    ptr to member: not working

--- module05/ex03/Intern.cpp
+++ module05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:59:26 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/11/28 19:47:27 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/11/28 19:51:42 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,26 +36,22 @@ Intern &Intern::operator= (const Intern &_Intern)
 
 Form *Intern::createShrubberyCreationForm(std::string target)
 {
-    this->Forms[0] = new ShrubberyCreationForm(target);
-    return this->Forms[0];
+    return new ShrubberyCreationForm(target);
 };
 
 Form *Intern::createRobotomyRequestForm(std::string target)
 {
-  this->Forms[1] = new RobotomyRequestForm(target);
-  return this->Forms[1];  
+    return new RobotomyRequestForm(target);  
 };
 
 Form *Intern::createPresidentialPardonForm(std::string target)
 {
-  this->Forms[2] = new PresidentialPardonForm(target);
-  return this->Forms[2];  
+    return  new PresidentialPardonForm(target); 
 };
 
 // typedef Form*(Intern::*PtrToMem[])(std::string _target);
 Form *Intern::makeForm(std::string formName, std::string target)
 {
-    (void)target;
     std::string _formName[3] = {"ShrubberyCreationForm", "RobotomyRequestForm", "PresidentialPardonForm"};
     Form*(Intern::*PtrToMem[])(std::string _target) = {&Intern::createShrubberyCreationForm, &Intern::createRobotomyRequestForm, &Intern::createPresidentialPardonForm};
     int i = -1;
