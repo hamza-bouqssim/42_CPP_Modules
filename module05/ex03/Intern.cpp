abb8577bc9563d75c25606a5431818e1f2b39845
bouqssim1 <hamzaboukssim6@gmail.com>
2022-11-28 14:56:30 +0100

    started imp...

---
+++ module05/ex03/Intern.cpp
@@ -0,0 +1,10 @@
+#include "Intern.hpp"
+Intern::Intern(){};
+
+Intern::Intern(const Intern &_Intern){};
+
+Intern &Intern::operator= (const Intern &_Intern){};
+
+void *Intern::makeForm(std::string formName, std::string target){};
+
+Intern::~Intern(){};
