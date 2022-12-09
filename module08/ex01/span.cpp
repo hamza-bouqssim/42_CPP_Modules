50661043d55e3d55d72d2b977b8f3320602f3f12
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-12-09 18:40:13 +0100

    Span: not done yet

--- module08/ex01/span.cpp
+++ module08/ex01/span.cpp
@@ -6,7 +6,7 @@
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 11:11:57 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/12/09 12:46:56 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/12/09 13:12:02 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,8 @@ void Span::addNumber(int number)
     if(this->numbers.size() >= this->N)
         throw "Error";
     this->numbers.push_back(number);
-}
+};
+int Span::longestSpan()
+{
+    
+};
