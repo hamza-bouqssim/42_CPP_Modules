9fc13bafa0f7c55b9cc24f8926daa04139cb0426
Hamza Bouqssim <hbouqssi@student.1337.ma>
2022-12-10 23:08:59 +0100

    implemented: shortestSpan() && longestSpan()

--- module08/ex01/main.cpp
+++ module08/ex01/main.cpp
@@ -6,13 +6,26 @@
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 11:11:52 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/12/09 11:57:52 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/12/10 23:04:11 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "span.hpp"
+#include "Span.hpp"
 int main()
-{
-    
+{   try
+    {
+        Span sp = Span(5);
+        sp.addNumber(3);
+        sp.addNumber(8);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        sp.addNumber(17);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+    }catch(const char *e)
+    {
+        std::cout << e << std::endl;
+    }
+        
     return 0;
 }
--- module08/ex01/span.cpp
+++ module08/ex01/span.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   span.cpp                                           :+:      :+:    :+:   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbouqssi <hbouqssi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 11:11:57 by hbouqssi          #+#    #+#             */
-/*   Updated: 2022/12/09 13:12:02 by hbouqssi         ###   ########.fr       */
+/*   Updated: 2022/12/10 23:07:37 by hbouqssi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,43 @@ void Span::addNumber(int number)
 };
 int Span::longestSpan()
 {
+    int longSpan;
+    int bigger;
+    bigger = 0;
+    if(this->numbers.size() < 2)
+        throw "Span contains less than two numbers";
+    std::vector<int>::iterator it1, it2;
+    std::sort(this->numbers.begin(), this->numbers.end());
+    // for(it =  this->numbers.begin() ; it != this->numbers.end(); it++)
+    //     std::cout << *it << " ";
+    // std::cout << std::endl;
+    it1 = this->numbers.begin();
+    it2 = this->numbers.end();
+    for (it1 = this->numbers.begin(); it1 != this->numbers.end(); it1++)
+        {
+            for (it2 = this->numbers.begin(); it2 != this->numbers.end(); it2++)
+            {
+                longSpan = (*it1 - *it2);
+                if (bigger < longSpan)
+                    bigger = longSpan;
+            }
+        }
+    return  bigger;
+}
+
+int Span::shortestSpan(){
+    if (this->numbers.size() < 2) {
+      throw "Span contains less than two numbers";
+    }
+    std::vector<int>_numbers = this->numbers;
+    std::sort(_numbers.begin(), _numbers.end());
+    int min_span = _numbers[1] - _numbers[0];
+    for (size_t i = 1; i < _numbers.size(); i++) {
+      int span = _numbers[i] - _numbers[i - 1];
+      if (span < min_span) {
+        min_span = span;
+      }
+    }
+    return min_span;
+  }
     
-};
