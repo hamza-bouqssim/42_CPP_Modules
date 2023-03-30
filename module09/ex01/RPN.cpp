b92df2330e9f040e596f18ac36975ce6e8e71f26
bouqssim1 <hamzaboukssim6@gmail.com>
2023-03-30 06:59:59 +0200

    fixed and finished

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -1,4 +1,5 @@
 #include "RPN.hpp"
+
 RPN::RPN()
 {
 
@@ -7,28 +8,38 @@ RPN::~RPN()
 {
 
 };
-void RPN::applyOperator(std::stack<int> &stack, char argument)
+
+RPN::RPN(RPN const &_RPN)
 {
-	int number1;
-	int number2;
-	if(stack.size() < 2)
-	{
-		std::cout << "Error ! Sorry :(" << std::endl;
-		return;
-	}
-	number1 = stack.top();
-	stack.pop();
-	number2 = stack.top();
-	stack.pop();
-	if (argument == '+')
-		stack.push(number1 + number2);
-	if (argument == '-')
-		stack.push(number1 - number2);
-	if (argument == '*')
-		stack.push(number1 * number2);
-	if (argument == '/')
-	{
-		if(number2 != 0)
-			stack.push(number1 / number2);
-	}	
+    *this = _RPN;
+};
+
+RPN &RPN::operator=(RPN const &_RPN)
+{
+	(void)_RPN;
+    return (*this);
+};
+
+void    RPN::applyOperator(std::stack<int> &stack, char argument)
+{
+    int   number1;
+    int   number2;
+
+    if (stack.size() < 2)
+    {
+        std::cout << "Error: Something Happened In Your Stack" << std::endl;
+        return;
+    }
+    number1 = stack.top();
+    stack.pop();
+    number2 = stack.top();
+    stack.pop();
+    if (argument == '*')
+        stack.push(number2 * number1);
+    if (argument == '/')
+        stack.push(number2 / number1);
+    if (argument == '+')
+        stack.push(number2 + number1);
+    if (argument == '-')
+        stack.push(number2 - number1);
 }
--- module09/ex01/main.cpp
+++ module09/ex01/main.cpp
@@ -1,34 +1,36 @@
 #include "RPN.hpp"
 int main(int argc, char **argv)
 {
-	if (argc == 2)
-	{
-		char *argument = argv[1];
-		int i = 0;
-		std::stack<int>stack;
-		while(argument[i])
-		{
-			if (argument[i] == ' ')
-				i++;
-			if (isdigit(argument[i]))
-				stack.push(atoi(&argument[i]));
-			else if (argument[i] == '+' || argument[i] == '-' || argument[i] == '*' || argument[i] == '/')
-				applyOperator(stack, argument[i]);
-			else
-			{
-				std::cerr << "Error" << std::endl;
-				return 0;
-			}
-			i++;
-		}
-		if (stack.size() != 1)
+    if (argc != 2)
+    {
+        std::cout << "Error: This Program Takes Only One Argument !" << std::endl;
+        return 0;
+    }
+        RPN rpn;
+        char *argument;
+        int i = -1;
+        std::stack<int> stack;
+        
+        argument = (argv[1]);
+        while(argument[++i])
         {
-            std::cerr << "Bad input" << std::endl;
+            if (argument[i] == ' ')
+                i++;
+            if (isdigit(argument[i]))
+                stack.push(atoi(&argument[i]));
+            else if (argument[i] == '+' || argument[i] == '-' || argument[i] == '*' || argument[i] == '/')
+                rpn.applyOperator(stack, argument[i]);
+            else
+            {
+                std::cout << "Error" << std::endl;
+                return (0);
+            }
+        }
+        if (stack.size() != 1)
+        {
+            std::cout << "Bad input" << std::endl;
             return (0);
         }
         std::cout << stack.top() << std::endl;
-	}
-	else
-		std::cerr << "Error: This Program Works With One Argument !" << std::endl;
-	return 0;
+    return 0;
 }
